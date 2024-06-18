cabb7631449faf42b2034f16e4aae4eaf403764b
uddhav <uddhavs5797@gmail.com>
2024-06-18 09:13:23 +0530

    Add project files.

---
+++ StudentPortal.Web/Controllers/StudentsController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentPortal.Web.Data;
+using StudentPortal.Web.Models;
+using StudentPortal.Web.Models.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentPortal.Web.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        //private readonly ApplicationDbContext dbContext;
+
+        public StudentsController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+            this.dbContext = dbContext;
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(AddStudentViewModel viewModel)
+        {
+            var student = new Student
+            {
+                Name = viewModel.Name,
+            Email = viewModel.Email,
+            Phone = viewModel.Phone,
+            subscribed= viewModel.subscribed
+        };
+            await dbContext.Students.AddAsync(student);
+            await dbContext.SaveChangesAsync();
+            return View();
+            
+        }
+        public async Task<IActionResult> List()
+        {
+            var students = await dbContext.Students.ToListAsync();
+            return View(students);
+        }
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var student = await dbContext.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+            return View(student);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, Student student)
+        {
+            if (id != student.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    dbContext.Update(student);
+                    await dbContext.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StudentExists(student.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(List));
+            }
+            return View(student);
+        }
+
+        private bool StudentExists(Guid id)
+        {
+            return dbContext.Students.Any(e => e.Id == id);
+        }
+
+
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var student = await dbContext.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+
+        [HttpPost]//, ActionName("Delete")
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var student = await dbContext.Students.FindAsync(id);
+            if (student != null)
+            {
+                dbContext.Students.Remove(student);
+                await dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(List));
+        }
+
+    }
+}
